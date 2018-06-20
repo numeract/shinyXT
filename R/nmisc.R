f5a08af117a0cab08324d29445c3e68af723de56
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-06-20 18:25:28 +0300

    Added up to date Nmisc functions + documentation for format_utc

--- R/nmisc.R
+++ R/nmisc.R
@@ -24,6 +24,25 @@ qval <- function(x, default = NULL, envir = parent.frame()) {
 is_POSIXct <- function(x) inherits(x, "POSIXct")
 
 
+#' Format Date and POSIXct
+#' 
+#' Converts Date and POSIXct objects to the format given as input.
+#' 
+#' @param x A Date or POSIXct object to be converted.
+#' 
+#' @param format A character string. The default format is
+#'   "\%Y-\%m-\%d" for Date and "\%Y-\%m-\%d \%H:\%M:\%S" for POSIXct.
+#' 
+#' @param usetz Logical. If TRUE, the time zone abbreviation is
+#'   appended to the output. Applicable only if an POSIXct object.
+#' 
+#' @return A character string representing the formatted date.
+#' 
+#' @seealso \code{\link{format.Date}}, \code{\link{format.POSIXct}}
+#' 
+#' @examples
+#' format_utc(Sys.time(), format = "%Y-%m-%d", usetz = FALSE)
+#' 
 #' @export
 format_utc <- function(x, format = NULL, usetz = TRUE) {
     
@@ -46,6 +65,9 @@ format_utc <- function(x, format = NULL, usetz = TRUE) {
 # purrr-misc ----
 keep_at <- function(.x, .at) {
     
+    if (length(.at) == 0L) return(.x[0L])
+    if (any(is.na(.at))) stop("`.at`` must not contain NA's")
+    
     .p <- if (is.character(.at)) {
         names(.x) %in% .at
     } else if (is.numeric(.at)) {
@@ -59,6 +81,9 @@ keep_at <- function(.x, .at) {
 
 
 discard_at <- function(.x, .at) {
+    
+    if (length(.at) == 0L) return(.x)
+    if (any(is.na(.at))) stop("`.at` must not contain NA's")
     
     .p <- if (is.character(.at)) {
         names(.x) %in% .at
